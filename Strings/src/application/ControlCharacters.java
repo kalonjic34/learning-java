1b1253003d598c6e77614e182edcb2161584451e
Christian <kalonjic34@gmail.com>
2024-02-13 13:32:05 +0200

    outputting a menu

--- Strings/src/application/ControlCharacters.java
+++ Strings/src/application/ControlCharacters.java
@@ -2,9 +2,26 @@ package application;
 
 public class ControlCharacters {
     public static void main(String[] args) {
-        String text = "To be or not to be. That is the question";
-        System.out.println(text);
-        \
-        
+        // String text = "\tTo be or not to be.\n\tThat is the question";
+
+        // System.out.println(text);
+
+        /*
+         * Select an option:
+         * 1. Add an entry
+         * 2. View the database
+         * 3. Exit
+         */
+
+        int one = 1;
+        int two = 2;
+        int three = 3;
+
+        String menu_text = "\tSelect an option:\n\t\t" + one + ". Add an entry\n\t\t" + two
+                + ". View the database\n\t\t"
+                + three
+                + ". Exit";
+        System.out.println(menu_text);
+
     }
 }
