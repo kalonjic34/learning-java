78a4dd6205279653d0175085ef70ce21061309ed
Christian <kalonjic34@gmail.com>
2024-02-13 14:22:23 +0200

    user input

--- Strings/src/application/FormatSpecifiers.java
+++ Strings/src/application/FormatSpecifiers.java
@@ -1,15 +1,19 @@
 package application;
 
+import java.util.Scanner;
+
 public class FormatSpecifiers {
     public static void main(String[] args) {
-        double fahrenheit = 91;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter value in fahrenheit to convert > ");
+
+        double fahrenheit = scan.nextFloat();
+        scan.close();
 
         double celcius = (fahrenheit - 32) * 5.0 / 9.0;
-        System.out.printf("%.1f degrees fahrenheit is %.2f degrees celcius.", fahrenheit, celcius);
+        System.out.printf("%.1f degrees fahrenheit is %.2f degrees celcius.",
+                fahrenheit, celcius);
 
-        // double value = 1.2343634;
-        // System.out.printf("Hello %.2f\n", value);
-        // System.out.printf("Hello %d\n", 123);
-        // System.out.printf("%s %d \n", "hello", 123);
     }
 }
