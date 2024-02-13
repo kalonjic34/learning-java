2ebc99a57097b4ced4b5ae26517090b1fbafda60
Christian <kalonjic34@gmail.com>
2024-02-13 13:13:19 +0200

    experimenting with concatination

--- Strings/src/application/App.java
+++ Strings/src/application/App.java
@@ -2,8 +2,8 @@ package application;
 
 public class App {
     public static void main(String[] args) {
-        int cats = 7;
-        double height = 5.3;
+        // int cats = 7;
+        // double height = 5.3;
 
         String name = "Chris";
 
