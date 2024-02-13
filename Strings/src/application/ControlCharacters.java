eac65c453526de6b435df5c822535e969a76ccee
Christian <kalonjic34@gmail.com>
2024-02-13 13:31:07 +0200

    outputting a menu

---
+++ Strings/src/application/ControlCharacters.java
@@ -0,0 +1,10 @@
+package application;
+
+public class ControlCharacters {
+    public static void main(String[] args) {
+        String text = "To be or not to be. That is the question";
+        System.out.println(text);
+        \
+        
+    }
+}
