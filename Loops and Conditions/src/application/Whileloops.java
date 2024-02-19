dd9ef504de0f167fbfd1ef6e31b1398d227d6ea4
Christian <kalonjic34@gmail.com>
2024-02-19 21:20:17 +0200

    loops, increments and decrements

---
+++ Loops and Conditions/src/application/Whileloops.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Whileloops {
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hello");
+        }
+
+    }
+
+}
