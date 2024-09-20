71edc823e4f134b95a03e5f54e9dd342e3d1ef1f
Christian <kalonjic34@gmail.com>
2024-09-20 17:19:51 +0200

    codes removed

--- Formatting/src/application/App.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello.");
-        System.out.println("How are you?");
-        System.out.println("I am a computer");
-    }
-}
