1f76f8e65732ffd09ec7fe17c29d305542282693
Christian <kalonjic34@gmail.com>
2024-09-23 11:24:39 +0200

    Java fundamentals: variables, math operations, and calculations

---
+++ 01 - Using Variables/Calculations/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+
+    /*
+     * Calculations
+     */
+
+    public static void main(String[] args) {
+        double personHeight = 1.67;
+        double flagpoleHeight = 2.0;
+
+        double totalHeight = (personHeight * 7) + flagpoleHeight;
+
+        System.out.println(totalHeight);
+    }
+}
