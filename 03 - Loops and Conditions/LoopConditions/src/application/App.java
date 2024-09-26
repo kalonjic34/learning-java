24f32cb8e34e396e06aa369fec8d2f0fc853e712
Christian <kalonjic34@gmail.com>
2024-09-26 16:13:10 +0200

    Loop conditions

---
+++ 03 - Loops and Conditions/LoopConditions/src/application/App.java
@@ -0,0 +1,16 @@
+package application;
+
+public class App {
+    /*
+     * Loop conditions
+     */
+    public static void main(String[] args) {
+
+        int counter = 0;
+
+        while (counter < 10) {
+            System.out.printf("Counter: %d\n", counter);
+            counter += 1;
+        }
+    }
+}
