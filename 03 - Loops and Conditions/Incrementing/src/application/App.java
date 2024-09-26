55a8584c43b66f1d73b7c7d3185d34f678f31704
Christian <kalonjic34@gmail.com>
2024-09-26 16:28:09 +0200

    Increment and decrement

---
+++ 03 - Loops and Conditions/Incrementing/src/application/App.java
@@ -0,0 +1,39 @@
+package application;
+
+public class App {
+    /*
+     * Increment and decrement
+     */
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count + 1;
+
+        System.out.println(count);
+
+        count += 1;
+
+        System.out.println(count);
+
+        count++;
+
+        System.out.println(count);
+
+        ++count;
+
+        System.out.println(count);
+
+        count = count - 1;
+
+        System.out.println(count);
+
+        count -= 1;
+
+        System.out.println(count);
+
+        count--;
+
+        System.out.println(count);
+
+    }
+}
