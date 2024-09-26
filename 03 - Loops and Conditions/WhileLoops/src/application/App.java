3a2500f86c0996b8b02c32df1c629d78c6816ef2
Christian <kalonjic34@gmail.com>
2024-09-26 16:01:24 +0200

    While loops introduction

---
+++ 03 - Loops and Conditions/WhileLoops/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+    /*
+     * While loops
+     */
+    public static void main(String[] args) {
+
+        while (true) {
+            System.out.println("Hello");
+        }
+
+    }
+}
