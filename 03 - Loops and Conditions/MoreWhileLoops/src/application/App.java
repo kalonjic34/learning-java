b5a802dbfdf26f5e982ccc0bc909d9a1cd5b4c44
Christian <kalonjic34@gmail.com>
2024-09-27 13:19:57 +0200

    More on while loops

---
+++ 03 - Loops and Conditions/MoreWhileLoops/src/application/App.java
@@ -0,0 +1,20 @@
+package application;
+
+public class App {
+    /*
+     * More on while loops
+     */
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 10) {
+            System.out.printf("Count is: %d\n", count);
+            count++;
+        }
+
+        int count1 = 7;
+        while (count1 > 2) {
+            System.out.printf("Count is %d\n", count1);
+            count1--;
+        }
+    }
+}
