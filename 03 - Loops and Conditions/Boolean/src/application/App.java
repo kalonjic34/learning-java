e0f243806ca15e37f2fed9a8f80a671c5311fd2d
Christian <kalonjic34@gmail.com>
2024-09-27 13:48:58 +0200

    Boolean

---
+++ 03 - Loops and Conditions/Boolean/src/application/App.java
@@ -0,0 +1,20 @@
+package application;
+
+public class App {
+    /*
+     * Boolean
+     */
+    public static void main(String[] args) {
+        boolean condition = true;
+        System.out.println("Condition 1: " + condition);
+
+        boolean condition2 = false;
+        System.out.println("Condition 2: " + condition2);
+
+        boolean condition3 = 4 < 5;
+        System.out.println("Condition 3: " + condition3);
+
+        boolean condition4 = 2 > 13;
+        System.out.println("Condition 4: " + condition4);
+    }
+}
