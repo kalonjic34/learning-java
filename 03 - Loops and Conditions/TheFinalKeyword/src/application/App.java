e6e95331d593c76b65da0351b68318a12fe2f51c
Christian <kalonjic34@gmail.com>
2024-10-01 16:44:03 +0200

    The final keyword

---
+++ 03 - Loops and Conditions/TheFinalKeyword/src/application/App.java
@@ -0,0 +1,17 @@
+package application;
+
+public class App {
+    /*
+     The final keyword
+     */
+    public static void main(String[] args) {
+        final int VALUE = 9;
+
+        System.out.println(VALUE);
+
+        final String USER_PASSWORD = "abc";
+        // userPassword = "edg";
+
+        System.out.println(USER_PASSWORD);
+    }
+}
