f2bee8b55a05ffd418f09ee8f39ea265847efc9b
Christian <kalonjic34@gmail.com>
2024-10-01 16:32:21 +0200

    Strings equals method

---
+++ 03 - Loops and Conditions/ComparingStrings/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+    public static void main(String[] args) {
+        /*
+        Strings equals method
+         */
+
+        String text1 = "orange";
+        String text2 = "apple";
+
+        System.out.println("Strings are identical: "+text1.equals(text2));
+    }
+}
