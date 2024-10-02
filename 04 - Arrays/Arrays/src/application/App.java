28e4419fb0f85f3cf2210214f91afa571d41e65d
Christian <kalonjic34@gmail.com>
2024-10-02 14:05:27 +0200

    Arrays introduction

---
+++ 04 - Arrays/Arrays/src/application/App.java
@@ -0,0 +1,24 @@
+package application;
+
+public class App {
+    /*
+     * Arrays 
+     */
+    public static void main(String[] args) {
+        
+        int[] numbers = {2,4,6,8};
+
+        /*
+         * 1. Tomatoes
+         * 2. Pasta
+         * 3. Onions
+         * 
+         * Note! Array indexes are zero-based
+         */
+
+         System.out.println(numbers[0]);
+         System.out.println(numbers[1]);
+         System.out.println(numbers[2]);
+         System.out.println(numbers[3]);
+    }
+}
