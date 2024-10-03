caa728fdf8137dcf7edba20deb1b0f2c2d32899e
Christian <kalonjic34@gmail.com>
2024-10-03 16:12:06 +0200

    Methods

---
+++ 05 - Methods/Methods/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+public class App {
+    /*
+     * Methods
+     */
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Chris";
+        
+        System.out.println(person1.name);
+        person1.sayHello();
+    }
+}
