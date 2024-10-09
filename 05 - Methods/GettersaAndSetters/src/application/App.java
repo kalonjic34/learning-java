73f3d9d21e99ab29481441336dfafdec671fa151
Christian <kalonjic34@gmail.com>
2024-10-09 15:36:52 +0200

    Getters and setters

---
+++ 05 - Methods/GettersaAndSetters/src/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+public class App {
+    /*
+     * Getters and setters
+     */
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.setName("Chris");
+        person1.setAge(24);
+
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
+    }
+}
