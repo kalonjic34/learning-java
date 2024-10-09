8940123d3fcc616b580896ea3d7789dbfe4d2723
Christian <kalonjic34@gmail.com>
2024-10-09 16:18:24 +0200

    Window example implementation

---
+++ 05 - Methods/WindowExample/src/application/App.java
@@ -0,0 +1,13 @@
+package application;
+
+public class App {
+    /*
+     * Window example implementation
+     */
+    public static void main(String[] args) {
+        Window window = new Window();
+
+        window.setHeight(300);
+        System.out.println(window.getHeight());
+    }
+}
---
+++ 05 - Methods/WindowExample/src/application/Window.java
@@ -0,0 +1,55 @@
+package application;
+
+public class Window {
+    private int width;
+    private int height;
+    private boolean isVisible;
+    private int top;
+    private int left;
+
+
+    public int getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public boolean isIsVisible() {
+        return this.isVisible;
+    }
+
+    public boolean getIsVisible() {
+        return this.isVisible;
+    }
+
+    public void setIsVisible(boolean isVisible) {
+        this.isVisible = isVisible;
+    }
+
+    public int getTop() {
+        return this.top;
+    }
+
+    public void setTop(int top) {
+        this.top = top;
+    }
+
+    public int getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(int left) {
+        this.left = left;
+    }
+
+}
