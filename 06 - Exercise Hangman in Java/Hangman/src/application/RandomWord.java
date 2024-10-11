d5b2cd374903d8c3fbbd8ee0ea84c25a1a7a0ab6
Christian <kalonjic34@gmail.com>
2024-10-11 14:06:35 +0200

    Char arrays

--- 06 - Exercise Hangman in Java/Hangman/src/application/RandomWord.java
+++ 06 - Exercise Hangman in Java/Hangman/src/application/RandomWord.java
@@ -7,13 +7,27 @@ public class RandomWord {
 
     private String chosenWord;
     private Random random = new Random();
+    private char[] characters;
 
 
     public RandomWord(){
         chosenWord = words[random.nextInt(words.length)];
+        characters = new char[chosenWord.length()];
     }
 
     public String toString(){
+        String text = "";
+
+        characters[1] = 'b';
+
+        for(char c: characters){
+            if(c=='\u0000'){
+                text += '_';
+            }else{
+                text += c;
+            }
+            text += ' ';
+        }
         return chosenWord;
     }
 }
