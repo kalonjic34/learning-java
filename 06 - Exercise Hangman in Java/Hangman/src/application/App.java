3901d386940a26f1617045aafac3edf3e7317f68
Christian <kalonjic34@gmail.com>
2024-10-11 14:20:12 +0200

    Removed comments

--- 06 - Exercise Hangman in Java/Hangman/src/application/App.java
+++ 06 - Exercise Hangman in Java/Hangman/src/application/App.java
@@ -1,9 +1,6 @@
 package application;
 
 public class App {
-    /*
-     * 1. Top down design
-     */
     public static void main(String[] args) {
         Hangman game = new Hangman();
         game.run();
