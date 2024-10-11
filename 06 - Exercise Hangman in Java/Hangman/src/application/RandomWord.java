acf55c9222114245104612fa52189bfc5253946e
Christian <kalonjic34@gmail.com>
2024-10-11 13:26:31 +0200

    Randomly choose a word from array

--- 06 - Exercise Hangman in Java/Hangman/src/application/RandomWord.java
+++ 06 - Exercise Hangman in Java/Hangman/src/application/RandomWord.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Random;
+
 public class RandomWord {
     private String[] words = {"father", "family", "infant", "tongue", "nothing", "longer","explicit","authority","tombstone", "blacksmith","likeness","photographs", "regarding","inscription"};
 
     private String chosenWord;
+    private Random random = new Random();
+
 
     public RandomWord(){
-        chosenWord = words[1];
+        chosenWord = words[random.nextInt(words.length)];
     }
 
     public String toString(){
