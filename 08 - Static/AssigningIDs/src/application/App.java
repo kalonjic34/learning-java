7b1753e5545494eeec2694aab7897cb298b12032
Christian <kalonjic34@gmail.com>
2024-10-14 16:33:26 +0200

    Assigning ids

---
+++ 08 - Static/AssigningIDs/src/application/App.java
@@ -0,0 +1,29 @@
+package application;
+
+public class App {
+    /*
+     * Assigning IDs
+     */
+    public static void main(String[] args) {
+
+        final String CAT_FORMAT_STR = "Count of cats: %d\n";
+
+        String catCount = String.format("Count of cats: %d\n", Cat.getCount());
+        System.out.println(catCount);
+
+        Cat cat1 = new Cat("Horace");
+        Cat cat2 = new Cat("Jamie");
+        Cat cat3 = new Cat("James");
+
+        System.out.println(cat1);
+        System.out.println(cat2);
+        System.out.println(cat3);
+
+        System.out.println(Cat.FOOD);
+
+        System.out.println(Cat.getCount());
+
+        catCount = String.format(CAT_FORMAT_STR, Cat.getCount());
+        System.out.println(catCount);
+    }
+}
