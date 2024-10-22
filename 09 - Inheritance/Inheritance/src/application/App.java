044ebb98318cca9df20d6b4fbbddbe14a632dbdf
Christian <kalonjic34@gmail.com>
2024-10-22 18:02:49 +0200

    Overriding methods

--- 09 - Inheritance/Inheritance/src/application/App.java
+++ 09 - Inheritance/Inheritance/src/application/App.java
@@ -2,14 +2,25 @@ package application;
 
 public class App {
     /*
-     * Inheritance 
+     * 1. Inheritance
+     * 2. Overriding Methods
      */
     public static void main(String[] args) {
+        /* 
         Animals animal1 = new Animals();
         animal1.eat();
 
         Bird bird1 = new Bird();
         bird1.eat();
         bird1.latEgg();
+        */
+
+        HouseCat cat1 = new HouseCat();
+        cat1.vocalize();
+        cat1.hunt();
+
+        Tiger cat2 = new Tiger();
+        cat2.vocalize();
+        cat2.hunt();
     }
 }
